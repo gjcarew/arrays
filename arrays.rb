3103a5a597d04bfbe75ec22244376f7a80165738
Gavin Carew <gjcarew@gmail.com>
2022-06-08 09:09:35 -0700

    Initial commit

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+# Array of strings
+skis = ["Scott", "Line", "ON3P", "Shot"]
+
+#Array of integers
+ski_width = [95, 108, 116, 88]
+
+#Array of floats
+ski_weight_kg = [2.125, 1.605, 2.450, 1.425]
+
+#Array of booleans
+ski_fun = [FALSE, TRUE, TRUE, FALSE]
